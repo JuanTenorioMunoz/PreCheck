a7b26a7d98e3b109c733d3a3b223bde4de33ebb8
Camilo Game <tenorio2003@gmail.com>
2025-03-10 13:24:43 -0500

    fix_price

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,30 @@
-import { useEffect, useState } from 'react'
-import ProductList from './components/ProductList/ProductList'
-import Cart from './components/Cart/Cart';
-import './App.css'
+  import { useEffect, useState } from 'react'
+  import ProductList from './components/ProductList/ProductList'
+  import Cart from './components/Cart/Cart';
+  import './App.css'
 
-function App() {
+  function App() {
 
-  const [cart, setCart] = useState([]);
-  
-  useEffect(() => {
-    console.log(cart)
-  }, [cart]
-  )
+    const [cart, setCart] = useState([]);
+    
+    useEffect(() => {
+      console.log(cart)
+    }, [cart]
+    )
 
-  const addToCart = (productInfo) => {
-    setCart((prevCart) => {
-      const updatedCart = [...prevCart, productInfo]
-      return updatedCart
-    });
+    const addToCart = (productInfo) => {
+      setCart((prevCart) => {
+        const updatedCart = [...prevCart, productInfo]
+        return updatedCart
+      });
+    }
+
+    return (
+      <>
+      <Cart cartItems={cart}></Cart>
+      <ProductList addToCart={addToCart}></ProductList>
+      </>
+    )
   }
 
-  return (
-    <>
-    <Cart cartItems={cart}></Cart>
-    <ProductList addToCart={addToCart}></ProductList>
-    </>
-  )
-}
-
-export default App
+  export default App
--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,6 +1,22 @@
+import { useEffect, useState } from "react";
+
 const Cart = ({cartItems}) => {
+
+    const [cartTotal, setCartTotal] = useState(0);
+    
+    useEffect(
+        ()=> {
+            const total = cartItems.length > 0 ? cartItems.reduce((sum, item) => sum + item.price, 0) : "fuckyou";
+            setCartTotal(total);
+        }, [cartItems]
+    )
+
     return(
         <div className="Cart">
+        <div>
+            <p>Total</p>
+            <p>{cartTotal}</p>
+        </div>
             {cartItems.map((item)=> (
                 <div>{item.name}</div>
             ))}
